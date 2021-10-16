faa65f96afb5638dd3af740228b61af0229165a5
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-16 15:39:01 +0330

    update structure

--- src/dhcpcli.c
+++ src/dhcpcli.c
@@ -1,18 +1,20 @@
 /**
  * @file dhcpcli.c
  * @author alirezaarzehgar (alirezaarzehgar82@gmail.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2021-10-16
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
 #include "cli/dhcpcli.h"
 
-int main(int argc, char const *argv[])
+int
+main (int argc, char const *argv[])
 {
-    printf("hello cli\n");
-    return 0;
+  printf ("hello cli\n");
+  printf ("path : %s\n", DHCP_DATABASE_PATH);
+  return 0;
 }
