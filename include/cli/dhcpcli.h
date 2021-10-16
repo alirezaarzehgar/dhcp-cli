82990f0142d1ab9cec6ca5d454a04f7d4dc9f45c
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-16 15:25:21 +0330

    base

---
+++ include/cli/dhcpcli.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if !defined(DHCPCLI_H)
+#define DHCPCLI_H
+
+
+
+#endif // DHCPCLI_H
---
+++ src/dhcpcli.c
@@ -0,0 +1,18 @@
+/**
+ * @file dhcpcli.c
+ * @author alirezaarzehgar (alirezaarzehgar82@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-10-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "cli/dhcpcli.h"
+
+int main(int argc, char const *argv[])
+{
+    printf("hello cli\n");
+    return 0;
+}
