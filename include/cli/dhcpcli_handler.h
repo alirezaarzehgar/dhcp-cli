42176fec528fd3b2544f056aa72154dae5e3a5bd
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-17 17:12:38 +0330

    dhcpcli_handler file structure

---
+++ include/cli/dhcpcli_handler.h
@@ -0,0 +1,9 @@
+#include "lease/lease.h"
+#include "cli/dhcpcli.h"
+
+#if !defined(DHCPCLI_HANDLER_H)
+#define DHCPCLI_HANDLER_H
+
+
+
+#endif // DHCPCLI_HANDLER_H
---
+++ src/dhcpcli_handler.c
@@ -0,0 +1,13 @@
+/**
+ * @file dhcpcli_handler.c
+ * @author alirezaarzehgar (alirezaarzehgar82@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-10-17
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include "cli/dhcpcli_handler.h"
+
