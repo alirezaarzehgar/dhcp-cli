2ce45c88f6f3df8a620da967305932ebf54fe3de
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-18 09:43:16 +0330

    Implement dhcpcliConfigShow

--- src/dhcpcli_handler.c
+++ src/dhcpcli_handler.c
@@ -13,17 +13,44 @@
 #include "cli/file.h"
 
 void
+printConf (dhcpLeaseConfigResult_t cfg)
+{
+  if (cfg.id == 0)
+    return;
+
+  printf ("id : %d\n", cfg.id);
+  printf ("mask : %s\n", cfg.mask);
+  printf ("router : %s\n", cfg.mask);
+  printf ("domain : %s\n", cfg.domain);
+  printf ("lease time : %d\n", cfg.lease_time);
+  printf ("\n");
+}
+
+void
 dhcpcliConfigShow (char *arg)
 {
-    /*TODO*/
-  if (arg == NULL)
-    {
-      dhcpLeaseConfigResult_t conf;
+  int id = atoi (arg != NULL ? arg : "");
 
-      for (int i = 1; conf.id > 0; conf = dhcpLeaseGetConfigById (i), i++)
-        printf ("%dmask : %s\n", conf.mask);
+  dhcpLeaseConfigResult_t conf;
+  int i = 1;
+
+  if (id > 0)
+    {
+      printConf (
+        dhcpLeaseGetConfigById (id)
+      );
+
+      return;
     }
 
+
+  do
+    {
+      bzero (&conf, sizeof (dhcpLeaseConfigResult_t));
+      conf = dhcpLeaseGetConfigById (i);
+      printConf (conf);
+    }
+  while (conf.id == i++);
 }
 
 void
