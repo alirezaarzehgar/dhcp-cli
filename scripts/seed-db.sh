339a700a66da56f1286e291ecc38994d353c9359
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-20 18:45:48 +0330

    Randomize values

--- scripts/seed-db.sh
+++ scripts/seed-db.sh
@@ -3,19 +3,36 @@
 EXECUTABLE=./$1
 FAKE_DB=test.db
 
+function randomMac() {
+    echo -n 00:60:2F; dd bs=1 count=3 if=/dev/random 2>/dev/null |hexdump -v -e '/1 ":%02X"'
+}
+
+function randomStr()
+{
+    dd bs=1 count=6 if=/dev/random 2>/dev/null |hexdump -v -e '/1 "%x"'
+}
+
+function randomUserName() {
+    echo $(randomStr)
+}
+
+function randomDomain() {
+    echo $(randomStr).com
+}
+
 function printConfigData() {
     echo 255.255.255.0
-    echo 192.168.133.2
-    echo example.com
+    echo 192.168.133.$1
+    echo $(randomDomain)
     echo
     echo Y
 }
 
 function printLeaseData() {
-    echo "ali"
-    echo 2
-    echo 192.168.133.2
-    echo
+    echo $(randomUserName)
+    echo $(echo $((RANDOM%6)))
+    echo $(randomMac)
+    echo 192.168.133.$1
     echo
     echo Y
 }
@@ -25,10 +42,10 @@ $EXECUTABLE -i$FAKE_DB 1&>2 /dev/null
 
 for i in `seq 5`;
 do
-    printConfigData | $EXECUTABLE -f $FAKE_DB --add --config 1&>2 /dev/null
+    printConfigData $i | $EXECUTABLE -f $FAKE_DB --add --config 1&>2 /dev/null
 done
 
 for i in `seq 20`;
 do
-    printLeaseData | $EXECUTABLE -f $FAKE_DB --add --lease 1&>2 /dev/null
+    printLeaseData $i | $EXECUTABLE -f $FAKE_DB --add --lease 1&>2 /dev/null
 done
